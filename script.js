e4b617068d8c52ae5645da03b1a1fa71286d9887
Renars Varslavans <renarsvars@gmail.com>
2022-05-09 23:24:51 +0300

    added font and resposivity

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const icon_plus = document.querySelector(".icons-faq")
 const top_icon = document.querySelector(".top-span-faq")
 const checkbox = document.querySelector(".faq-input")
+const hidden_faq = document.querySelector(".faq-p")
 
 icon_plus.onclick = function show_content() {
     checkbox.checked = !checkbox.checked
@@ -8,8 +9,11 @@ icon_plus.onclick = function show_content() {
     if(checkbox.checked == true){
         top_icon.style.transform = "rotate(180deg)"
         top_icon.style.transform = "translateY(2px)"
-        
+        hidden_faq.style.display = "inline"
+        hidden_faq.style.translate = "translate(150px)"
     }else{
         top_icon.style.transform = "rotate(90deg)"
+        hidden_faq.style.display = "none"
+        hidden_faq.style.translate = "translate(-150px)"
     }
 }
