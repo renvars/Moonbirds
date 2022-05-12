89a5cfcb5fce08197c3a301a67383e74a02fc093
Renars Varslavans <renarsvars@gmail.com>
2022-05-12 12:04:45 +0300

    final changes to design

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const icon_plus = document.querySelector(".icons-faq")
-const top_icon = document.querySelector(".top-span-faq")
-const checkbox = document.querySelector(".faq-input")
-const hidden_faq = document.querySelector(".faq-p")
-
-icon_plus.onclick = function show_content() {
-    checkbox.checked = !checkbox.checked
-    console.log(checkbox.checked)
-    if(checkbox.checked == true){
-        top_icon.style.transform = "rotate(180deg)"
-        top_icon.style.transform = "translateY(2px)"
-        hidden_faq.style.display = "inline"
-        hidden_faq.style.translate = "translate(150px)"
-    }else{
-        top_icon.style.transform = "rotate(90deg)"
-        hidden_faq.style.display = "none"
-        hidden_faq.style.translate = "translate(-150px)"
+const icon_plus = document.querySelectorAll(".icons-faq")
+const top_icon = document.querySelectorAll(".top-span-faq")
+const checkbox = document.querySelectorAll(".faq-input")
+const hidden_faq = document.querySelectorAll(".faq-p")
+for (let i = 0;i < icon_plus.length;i++){
+    icon_plus[i].onclick = function show_content() {
+        checkbox[i].checked = !checkbox[i].checked
+        if(checkbox[i].checked == true){
+            top_icon[i].style.transform = "rotate(180deg)"
+            top_icon[i].style.transform = "translateY(2px)"
+            hidden_faq[i].style.display = "inline"
+            hidden_faq[i].style.translate = "translate(150px)"
+        }else{
+            top_icon[i].style.transform = "rotate(90deg)"
+            hidden_faq[i].style.display = "none"
+            hidden_faq[i].style.translate = "translate(-150px)"
+        }
     }
 }
+
